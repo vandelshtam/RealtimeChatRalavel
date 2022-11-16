53bc0702b5f8b55d724c89df8ce3bd4fd989d595
vandelshtam <mvlju977@gmail.com>
2022-11-16 20:55:43 +0300

    pusher, echo-laravel, nitification

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -6,6 +6,16 @@
     </x-slot>
 
     <div class="py-12">
+        <div class="alert alert-danger max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div id="notification" class="alert overflow-hidden shadow-sm sm:rounded-lg alert-danger">
+                <div class="p-6 text-gray-900">
+                    You're logged in!
+                </div>
+            </div>
+            @yield('contetnt')
+        </div>
+    </div>
+    <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
